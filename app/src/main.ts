57c2c1f887fee7019541251439c70c202c3cae91
lucas rodrigues <lucasrodrigues062@gmail.com>
2023-08-26 12:33:58 -0300

    Ajustado porta da aplicacao

--- app/src/main.ts
+++ app/src/main.ts
@@ -15,6 +15,6 @@ async function bootstrap() {
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   const document = SwaggerModule.createDocument(app, swaggerConfig);
   SwaggerModule.setup('swagger', app, document);
-  await app.listen(3000);
+  await app.listen(process.env.APP_PORT || 3000);
 }
 bootstrap();
